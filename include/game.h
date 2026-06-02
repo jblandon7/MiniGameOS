c11ce3638277eb53b9a63fd755255ba9c78fcdc2
jblandon7 <156246212+jblandon7@users.noreply.github.com>
2026-06-01 22:16:55 -0400

    Add menu and game over flow

--- include/game.h
+++ include/game.h
@@ -1,9 +1,10 @@
 #ifndef GAME_H
 #define GAME_H
 
-void game_init(void);
+void game_init(int game_number);
 void game_update(void);
 void game_draw(void);
+int game_is_over(void);
 void game_run_demo(void);
 
 #endif
