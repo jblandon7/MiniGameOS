24c875b86f3773f7411c1412894f80b20548c133
jblandon7 <156246212+jblandon7@users.noreply.github.com>
2026-06-02 22:00:36 -0400

    Wire framebuffer graphics boot path

--- boot/kernel_entry.asm
+++ boot/kernel_entry.asm
@@ -2,28 +2,37 @@ section .multiboot
     ; GRUB checks this section to see if this file is a bootable kernel
 
     dd 0x1BADB002                              ; Password telling grub that this is a bootable kernel
-    dd 0x00                                    ; Special Instructions for GRUB, flag
-    dd -(0x1BADB002 + 0x00)                    ; security checksum, pswd + flag + checksum must equal 0 or else it wont run
+    dd 0x07                                    ; Align modules, request memory info, and ask for graphics mode
+    dd -(0x1BADB002 + 0x07)                    ; magic + flags + checksum must equal 0
+    dd 0                                       ; Header address, unused with these flags
+    dd 0                                       ; Load address, unused with these flags
+    dd 0                                       ; Load end address, unused with these flags
+    dd 0                                       ; BSS end address, unused with these flags
+    dd 0                                       ; Entry address, unused with these flags
+    dd 0                                       ; 0 means linear framebuffer graphics mode
+    dd 640                                     ; Preferred framebuffer width
+    dd 480                                     ; Preferred framebuffer height
+    dd 32                                      ; Preferred framebuffer depth
 
-
-section .bss					; Uninitialized memory/storage
+section .bss                                  ; Uninitialized memory/storage
 align 16
 stack_bottom:
-	resb 16384
+    resb 16384
 stack_top:
 
-section .text					;where CPU instructions go
-global start					; Mark of the kernel entry point visible to linker
-extern kernel_main				; kernel_main is defined in another file, kernel.c
+section .text                                 ; where CPU instructions go
+global start                                  ; Mark of the kernel entry point visible to linker
+extern kernel_main                            ; kernel_main is defined in kernel.c
 
-; Where CPU instructions start
+; GRUB puts a magic number in eax and the Multiboot info address in ebx.
 start:
     mov esp, stack_top
-    call kernel_main				; Assembly hands control of the C function kernel_main()
+    push ebx                                  ; Second C argument: multiboot info address
+    push eax                                  ; First C argument: multiboot magic number
+    call kernel_main                          ; Assembly hands control to kernel_main()
 
-
-; Stop for when kernel main finishes
+; Stop for when kernel_main finishes
 hang:
     cli
-    hlt						; Tells the CPU to go to sleep until interrupt wakes it up
-    jmp hang					; Loop back to tell the CPU to go back to sleep
+    hlt                                       ; Tells the CPU to sleep until an interrupt wakes it up
+    jmp hang                                  ; Loop back to sleep again
